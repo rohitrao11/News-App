faffe02a0c2ccd7a6087e08d0f0227a3c7daa69e
Rohit L Rao <rohitrao11062002@gmail.com>
2022-12-20 18:55:08 +0530

    adding news

---
+++ app/src/main/java/com/example/news/DetailsActivity.java
@@ -0,0 +1,59 @@
+package com.example.news;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.news.Models.NewsHeadlines;
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.Picasso;
+
+public class DetailsActivity extends AppCompatActivity {
+    NewsHeadlines headlines;
+    WebView web;
+    /*TextView txt_title, txt_author, txt_time, txt_detail, txt_content;
+    ImageView img_news;*/
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+
+        headlines = (NewsHeadlines) getIntent().getSerializableExtra("data");
+
+        web = findViewById(R.id.webView);
+        WebSettings webSettings = web.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        web.setWebViewClient(new Caller());
+        web.loadUrl(headlines.getUrl());
+
+        /*txt_title = findViewById(R.id.text_detail_title);
+        txt_author = findViewById(R.id.text_detail_author);
+        txt_time = findViewById(R.id.text_detail_time);
+        txt_detail = findViewById(R.id.text_detail_detail);
+        txt_content = findViewById(R.id.text_detail_content);
+        img_news = findViewById(R.id.img_detail_news);*/
+
+        //headlines = (NewsHeadlines) getIntent().getSerializableExtra("data");
+
+        /*txt_title.setText(headlines.getTitle());
+        txt_author.setText(headlines.getAuthor());
+        txt_time.setText(headlines.getPublishedAt());
+        txt_detail.setText(headlines.getDescription());
+        txt_content.setText(headlines.getContent());
+        Picasso.get().load(headlines.getUrlToImage()).into(img_news);*/
+    }
+
+    private class Caller extends WebViewClient {
+        @Override
+        public boolean shouldOverrideKeyEvent(WebView view, KeyEvent event) {
+            return false;
+        }
+    }
+}
